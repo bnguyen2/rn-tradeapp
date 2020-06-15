ec0d167352db25e7324bc3fde1e47159ea8218e7
Bao Nguyen <bao.cnguyen2@gmail.com>
2020-06-14 21:42:20 -0700

    refactor and restyle the register and signup screen

--- app/src/screens/SignupScreen/index.js
+++ app/src/screens/SignupScreen/index.js
@@ -1,13 +1,14 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { View, Button, Text } from 'react-native';
-import styled, { css } from 'styled-components/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { View, Text, Keyboard, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
+import styled, { css, ThemeContext } from 'styled-components/native';
 import AuthContext from 'context/authContext';
 
 const StyledTextInput = styled.TextInput`
   color: #fff;
-  font-size: 18px;
-  width: 200px;
+  font-size: 16px;
   height: 50px;
+  padding: 10px;
 `;
 
 const SignupScreen = ({ navigation }) => {
@@ -16,6 +17,8 @@ const SignupScreen = ({ navigation }) => {
     signup,
     clearErrorMessage,
   } = useContext(AuthContext);
+
+  const { colors } = useContext(ThemeContext);
 
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
@@ -28,59 +31,202 @@ const SignupScreen = ({ navigation }) => {
   }, [navigation]);
 
   return (
-    <View
-      css={css`
-        flex: 1;
-        background-color: #000;
-        align-items: center;
-        justify-content: center;
-      `}
-    >
-      <StyledTextInput
-        placeholder="First Name"
-        placeholderTextColor="#a8a8a8"
-        autoCorrect={false}
-        autoCapitalize="none"
-        onChangeText={(text) => setFirstName(text)}
-      />
-      <StyledTextInput
-        placeholder="Last Name"
-        placeholderTextColor="#a8a8a8"
-        autoCorrect={false}
-        autoCapitalize="none"
-        onChangeText={(text) => setLastName(text)}
-      />
-      <StyledTextInput
-        placeholder="Email Address"
-        placeholderTextColor="#a8a8a8"
-        autoCorrect={false}
-        autoCapitalize="none"
-        onChangeText={(text) => setEmail(text)}
-      />
-      <StyledTextInput
-        placeholder="Password"
-        placeholderTextColor="#a8a8a8"
-        autoCorrect={false}
-        secureTextEntry
-        autoCapitalize="none"
-        onChangeText={(text) => setPassword(text)}
+    <>
+      <SafeAreaView
+        css={css`
+          background-color: ${colors.darkNavy};
+        `}
       />
 
-      <Button title="Submit" onPress={() => signup(firstName, lastName, email, password)} />
-
-      <Button title="Dismiss" onPress={() => navigation.goBack()} />
-
-      {errorMessage ? (
-        <Text
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View
           css={css`
-            font-size: 18px;
-            color: red;
+            flex: 1;
+            background-color: ${colors.darkNavy};
+            align-items: flex-start;
+            padding-left: 10px;
+            padding-right: 10px;
           `}
         >
-          {errorMessage}
-        </Text>
-      ) : null}
-    </View>
+          <Text
+            css={css`
+              font-family: Roboto-Bold;
+              font-size: 30px;
+              color: ${colors.skyBlue};
+            `}
+          >
+            Register
+          </Text>
+        </View>
+      </TouchableWithoutFeedback>
+
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View
+          css={css`
+            flex: 10
+            background-color: ${colors.darkNavy};
+            padding-left: 10px;
+            padding-right: 10px;
+          `}
+        >
+          <View>
+            <Text
+              css={css`
+                font-size: 18px;
+                color: #fff;
+              `}
+            >
+              Email
+            </Text>
+          </View>
+
+          <View
+            css={css`
+              margin-top: 10px;
+              background-color: ${colors.navy};
+              border-radius: 10px;
+            `}
+          >
+            <StyledTextInput
+              placeholder="Email"
+              placeholderTextColor="#a8a8a8"
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={(text) => setEmail(text)}
+            />
+          </View>
+
+          <View
+            css={css`
+              margin-top: 10px;
+            `}
+          >
+            <Text
+              css={css`
+                font-size: 18px;
+                color: #fff;
+              `}
+            >
+              Password
+            </Text>
+          </View>
+
+          <View
+            css={css`
+              margin-top: 10px;
+              background-color: ${colors.navy};
+              border-radius: 10px;
+            `}
+          >
+            <StyledTextInput
+              placeholder="Password"
+              placeholderTextColor="#a8a8a8"
+              autoCorrect={false}
+              secureTextEntry
+              autoCapitalize="none"
+              onChangeText={(text) => setPassword(text)}
+            />
+          </View>
+
+          <View
+            css={css`
+              margin-top: 10px;
+            `}
+          >
+            <Text
+              css={css`
+                font-size: 18px;
+                color: #fff;
+              `}
+            >
+              First Name
+            </Text>
+          </View>
+
+          <View
+            css={css`
+              margin-top: 10px;
+              background-color: ${colors.navy};
+              border-radius: 10px;
+            `}
+          >
+            <StyledTextInput
+              placeholder="First Name"
+              placeholderTextColor="#a8a8a8"
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={(text) => setFirstName(text)}
+            />
+          </View>
+
+          <View
+            css={css`
+              margin-top: 10px;
+            `}
+          >
+            <Text
+              css={css`
+                font-size: 18px;
+                color: #fff;
+              `}
+            >
+              Last Name
+            </Text>
+          </View>
+
+          <View
+            css={css`
+              margin-top: 10px;
+              background-color: ${colors.navy};
+              border-radius: 10px;
+            `}
+          >
+            <StyledTextInput
+              placeholder="Last Name"
+              placeholderTextColor="#a8a8a8"
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={(text) => setLastName(text)}
+            />
+          </View>
+
+          <TouchableOpacity onPress={() => signup(firstName, lastName, email, password)}>
+            <View
+              css={css`
+                margin-top: 30px;
+                background-color: ${colors.skyBlue};
+                justify-content: center;
+                align-items: center;
+                height: 50px;
+                border-radius: 10px;
+              `}
+            >
+              <Text
+                css={css`
+                  font-family: Roboto;
+                  font-size: 22px;
+                  color: ${colors.darkNavy};
+                `}
+              >
+                Create an account
+              </Text>
+            </View>
+          </TouchableOpacity>
+
+          {errorMessage ? (
+            <Text
+              css={css`
+                margin-top: 10px;
+                font-size: 16px;
+                color: ${colors.pink};
+              `}
+            >
+              {errorMessage}
+            </Text>
+          ) : null}
+        </View>
+      </TouchableWithoutFeedback>
+    </>
   );
 };
 
